4313f4267117cb9841d6d6a8bafd72f4e188e568
ueeet <lehasellaki@gmail.com>
2026-03-26 22:40:36 +0300

    auto: update

--- backend/src/routes/artists.ts
+++ backend/src/routes/artists.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
 
 const prisma = new PrismaClient();
 export const artistRouter = Router();
