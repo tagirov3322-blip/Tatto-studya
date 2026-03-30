01448170d6b1310dfa6f203e3b42c5d6ab515b0c
ueeet <lehasellaki@gmail.com>
2026-03-30 16:35:03 +0300

    auto: update

--- backend/src/prismaClient.ts
+++ backend/src/prismaClient.ts
@@ -1,5 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({
+  host: "aws-1-eu-west-1.pooler.supabase.com",
+  port: 5432,
+  database: "postgres",
+  user: "postgres.ucqgyzuwvpjyfpwucdpe",
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
+});
 export const prisma = new PrismaClient({ adapter });
